ae4742d5bcfd19fedb15f320efa3d65c1b02838c
Skalski02 <skalski.szymon@wp.pl>
2026-01-23 12:01:32 +0100

    23.01 com2

--- programy.r/23.01.r
+++ programy.r/23.01.r
@@ -26,13 +26,13 @@ pingwiny2 <- penguins |>
 pingwiny3 <- penguins |>
   dplyr::select(species, bill_dep, sex) |>
   dplyr::filter(bill_dep > 20) |>
-  dplyr::filter(sex == 'male') |> 
+  dplyr::filter(sex == 'male') |>
   (\(x) mean(x$bill_dep))()
 
 pingwiny4 <- penguins |>
   dplyr::select(species, bill_dep, sex, .data = _) |>
   dplyr::filter(bill_dep > 20) |>
-  dplyr::filter(sex == 'male') |> 
+  dplyr::filter(sex == 'male') |>
   (\(x) mean(x$bill_dep))()
 
 #iris, wyrzucić wszystkie NA, funkcja z tidyr drop_na
@@ -44,3 +44,24 @@ kwiaty <- iris |>
   dplyr::filter(Species == 'setosa') |>
   dplyr::mutate(nowakolumna = Petal.Length / Petal.Width) |>
   dplyr::select(nowakolumna)
+
+
+#_____________________________________________
+#Tidyr
+
+#format ramek szeroki i długi
+
+kwiaty <- iris |>
+  tibble::rowid_to_column(var = "ID") |>
+  tidyr::pivot_longer(
+    cols = -c(ID, Species),
+    names_to = "nazwa",
+    values_to = "wartosc"
+  )
+
+ggplot2::ggplot(kwiaty, aes(ID, wartosc, color = nazwa, size = Species)) +
+  geom_point()
+
+ggplot2::ggplot(kwiaty, aes(ID, wartosc, color = nazwa)) +
+  geom_point() +
+  facet_wrap(vars(Species), scales = "free_x")
