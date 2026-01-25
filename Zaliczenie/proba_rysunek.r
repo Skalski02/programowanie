550d1b6053f8fce914b3edcd3798114eeb29eb39
Skalski02 <skalski.szymon@wp.pl>
2026-01-25 13:26:07 +0100

    kod zaliczenie + rysunek

--- Zaliczenie/proba_rysunek.r
+++ Zaliczenie/proba_rysunek.r
@@ -34,17 +34,8 @@ Snieg2 <- Snieg |>
   tidyr::pivot_longer(
     cols = c(sniegcmK, sniegcmR, sniegcmS),
     names_to = "nazwa",
-    values_to = "wartosc_cm"
+    values_to = "wartosc"
   )
 
-ggplot2::ggplot(Snieg2, aes(miesiac, wartosc_cm, color = nazwa,)) +
+ggplot2::ggplot(Snieg2, aes(miesiac, wartosc, color = nazwa,)) +
   geom_point()
-
-Snieg <- dplyr::summarise(Snieg,
-    dplyr::across(starts_with("snieg"), list(suma = \(x) sum(x, na.rm = TRUE)))
-  ) |>
-  dplyr::rename(
-    Kielce_suma_opadu_cm = sniegcmK_suma,
-    Szczecin_suma_opadu_cm = sniegcmS_suma,
-    Rzeszow_suma_opadu_cm = sniegcmR_suma
-  )
